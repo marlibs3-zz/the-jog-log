54cf8b1ffe96005d7be6e08eb2e92446f6e5c09b
Marta Beveridge <marta@beveridge.uk>
2019-11-13 02:48:53 +0000

    :whale::wrench: requirements and docker.

--- readme.md
+++ readme.md
@@ -2,7 +2,8 @@
 
 This simple Django web-app allows you to keep a record of your runs. It also calculates the calories that you burned during said exercise.
 
-To run it:
+To run the local dev server:
 - Clone the repo
-- Run `docker run`
+- Run `docker build -t jogger-dev-server`
+- Run `docker run -p 8000:8000 jogger-dev-server`
 - Navigate to http://127.0.0.1:8000/
--- thejoglog/tests.py
+++ thejoglog/tests.py
@@ -2,6 +2,9 @@ from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
 from .views import home
+from .views import new_log
+from .models import Log
+
 
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
@@ -9,7 +12,28 @@ class HomeTests(TestCase):
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
-
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+
+class NewLogTests(TestCase):
+    def setUp(self):
+        Log.objects.create(date='2019-11-01', distance=4.7, time=20, weight=80)
+
+    def test_csrf(self):
+        url = reverse('new_log')
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'date': '2019-11-01',
+            'distance': 4.9,
+            'time': 22,
+            'weight': 80,
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
