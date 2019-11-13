0c2cdb56749895a791cad36e90802614b8535d37
Marta Beveridge <marta@beveridge.uk>
2019-11-13 01:48:05 +0000

    :sparkles::lipstick: form styled and submittable.

--- thejoglog/views.py
+++ thejoglog/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Log
 
 
 def home(request):
     logs = Log.objects.all()
     return render(request, 'home.html', {'logs': logs})
+
+
+def new_log(request):
+    if request.method == 'POST':
+        date = request.POST['date']
+        distance = request.POST['distance']
+        time = request.POST['time']
+        weight = request.POST['weight']
+
+        log = Log.objects.create(
+            date=date,
+            distance=distance,
+            time=time,
+            weight=weight,
+        )
+
+        return redirect('home')
+
+    return render(request, 'new_log.html')
